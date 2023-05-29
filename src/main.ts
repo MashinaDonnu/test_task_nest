7cd68e5f9fb3dca3afa8aeb456701201a910fa7f
Dima Z <dmitriy.zahatin@asta.mobi>
2023-05-29 13:29:35 +0300

    fix port logs

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,10 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const PORT = process.env.PORT;
   console.log('process.env.PORT: ', PORT);
-  const config = await app.get(ConfigService);
 
-  //@ts-ignore
-  const port = +config.get<number>('PORT') || 3000;
-
-  console.log('bootstrap port ', port);
   const app = await NestFactory.create(AppModule);
 
-  await app.listen(port);
-  console.log(`App started on port ${port}`);
+  await app.listen(PORT);
+  console.log(`App started on port ${PORT}`);
 }
 bootstrap().catch((e) => console.log('Bootstrap error: ', e));
