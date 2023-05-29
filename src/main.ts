24398f9d98893ce323a1b0f6bf9a377145db9eca
Dima Z <dmitriy.zahatin@asta.mobi>
2023-05-29 14:14:30 +0300

    rollback app listen

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // await app.listen(PORT);
-  await app.listen(process.env.PORT || 3001, '0.0.0.0', () => {
-    console.log(`App started on port callback ${PORT}`, app);
-  });
+  await app.listen(process.env.PORT || 3000);
   console.log(`App started on port ${PORT}`);
 }
 bootstrap().catch((e) => console.log('Bootstrap error: ', e));
