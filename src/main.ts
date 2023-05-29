574e1ad9d11f2bb781d3ead26c1652b68bbf78e0
Dima Z <dmitriy.zahatin@asta.mobi>
2023-05-29 05:30:05 +0300

    fix DOckerfile

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = await app.get(ConfigService);
-  console.log('process env port: ', process.env.PORT);
   //@ts-ignore
   const port = +config.get<number>('PORT') || 3000;
-  console.log('port');
 
   await app.listen(port);
   console.log(`App started on port ${port}`);
